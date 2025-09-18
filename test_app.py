50ce10a1b60b5cc4ab693f8ac6c43d94f854d4de
vijaykankane <vijay.kankane@gmail.com>
2025-09-18 12:29:51 +0530

    intial commit

---
+++ test_app.py
@@ -0,0 +1,17 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_hello(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello, Jenkins CI/CD!" in response.data
+
+def test_health(client):
+    response = client.get('/health')
+    assert response.status_code == 200
